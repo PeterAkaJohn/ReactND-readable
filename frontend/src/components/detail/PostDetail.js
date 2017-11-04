1429cebe1f0e589b679668cbeaa9672184364075
PierpaoloIannone <piannone@byte-code.com>
2017-11-04 14:17:47 +0100

    chore: refactor PostDetail in Post and Comment sections

--- frontend/src/components/detail/PostDetail.js
+++ frontend/src/components/detail/PostDetail.js
@@ -2,18 +2,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import withPost from '../hoc/withPost';
+import PostSection from './posts/PostSection';
+import CommentSection from './comments/CommentSection';
 
 function PostDetail(props) {
   return (
     <div>
-      <div>{props.post.title}</div>
-      <ol>{props.post.comments && props.post.comments.map(comment => <li>{comment.body}</li>)}</ol>
+      <PostSection {...props} />
+      <CommentSection {...props} />
     </div>
   );
 }
 
 PostDetail.propTypes = {
-  post: PropTypes.shape({ title: PropTypes.string, comments: PropTypes.array }).isRequired,
+  post: PropTypes.shape({
+    title: PropTypes.string,
+    body: PropTypes.string,
+    voteScore: PropTypes.number,
+    comments: PropTypes.array,
+    id: PropTypes.string,
+  }).isRequired,
 };
 
 export default withPost(PostDetail);
