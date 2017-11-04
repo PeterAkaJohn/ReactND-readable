a7db9342cf8846c9afe0baa428093fe86bbfe10f
PierpaoloIannone <piannone@byte-code.com>
2017-11-04 15:28:03 +0100

    chore: Manage route correctly for postdetail

--- frontend/src/components/detail/PostDetail.js
+++ frontend/src/components/detail/PostDetail.js
@@ -1,14 +1,23 @@
 // connect to store
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Route, Switch } from 'react-router-dom';
 import withPost from '../hoc/withPost';
 import PostSection from './posts/PostSection';
+import EditPostSection from './posts/EditPostSection';
 import CommentSection from './comments/CommentSection';
 
 function PostDetail(props) {
   return (
     <div>
-      <PostSection {...props} />
+      <Switch>
+        <Route
+          exact
+          path={`${props.match.url}/edit`}
+          render={() => <EditPostSection {...props} />}
+        />
+        <Route path={`${props.match.url}/`} render={() => <PostSection {...props} />} />
+      </Switch>
       <CommentSection {...props} />
     </div>
   );
@@ -22,6 +31,7 @@ PostDetail.propTypes = {
     comments: PropTypes.array,
     id: PropTypes.string,
   }).isRequired,
+  match: PropTypes.shape({ url: PropTypes.string }).isRequired,
 };
 
 export default withPost(PostDetail);
